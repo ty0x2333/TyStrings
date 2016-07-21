276f7da6107a5c89b4ff6be1ccfa67af5d96b02a
luckytianyiyan <luckytianyiyan@gmail.com>
2016-07-21 21:09:50 +0800

    A argparse support

--- tystrings.py
+++ tystrings.py
@@ -1,10 +1,30 @@
 # -*- encoding: utf-8 -*-
 import re
 import codecs
+import argparse
 __author__ = 'luckytianyiyan@gmail.com'
 
 
+def arg_parser():
+    description = r"""
+  _______     _____ _        _
+ |__   __|   / ____| |      (_)
+    | |_   _| (___ | |_ _ __ _ _ __   __ _ ___
+    | | | | |\___ \| __| '__| | '_ \ / _` / __|
+    | | |_| |____) | |_| |  | | | | | (_| \__ \
+    |_|\__, |_____/ \__|_|  |_|_| |_|\__, |___/
+        __/ |                         __/ |
+       |___/                         |___/
+
+    """
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=description)
+
+    return parser
+
+
 def main():
+    parser = arg_parser()
+    args = parser.parse_args()
     source = {}
     f = codecs.open('Localizable.strings', "r", encoding='utf_16')
     for line in f:
