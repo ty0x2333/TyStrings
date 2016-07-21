f9e706b2f55c528e80d0c168a18dcdabbc53a7bb
luckytianyiyan <luckytianyiyan@gmail.com>
2016-07-21 23:29:42 +0800

    M use genstrings generate strings file

--- tystrings.py
+++ tystrings.py
@@ -2,7 +2,30 @@
 import re
 import codecs
 import argparse
+import os
+import logging
+import subprocess
 __author__ = 'luckytianyiyan@gmail.com'
+
+
+def __run_script(script, display_output=True):
+    if display_output:
+        logging.info('%s' % script)
+        logging.info('---')
+    process = subprocess.Popen(script, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = ''
+    while process.poll() is None:
+        line = process.stdout.readline()
+        if line:
+            output += line
+            if display_output:
+                logging.info(line.strip())
+    if display_output:
+        logging.info('process finished with %s\n' % ('success' if
+                                                     process.returncode == 0 or process.returncode is None else
+                                                     ('exit code %r' % process.returncode)))
+
+    return process.returncode, output
 
 
 def arg_parser():
@@ -26,16 +49,23 @@ def arg_parser():
 def main():
     parser = arg_parser()
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG, format='%(message)s', )
+
     source = {}
-    f = codecs.open(args.filename, "r", encoding='utf_16')
-    for line in f:
-        match = re.match(r'"(?P<key>.*?)" = "(?P<value>.*?)";', line)
-        if match is not None:
-            # print match.group('key'), match.group('value')
-            source[match.group('key')] = match.group('value')
+    if os.path.isdir(args.filename):
+        parser.error('%s is a directory' % args.filename)
 
-    print source
+    __run_script('genstrings %s' % args.filename)
 
+    # f = codecs.open(args.filename, "r", encoding='utf_16')
+    # for line in f:
+    #     match = re.match(r'"(?P<key>.*?)" = "(?P<value>.*?)";', line)
+    #     if match is not None:
+    #         # print match.group('key'), match.group('value')
+    #         source[match.group('key')] = match.group('value')
+    #
+    # print source
 
 if __name__ == '__main__':
     main()
