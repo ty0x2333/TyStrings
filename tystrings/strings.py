ff32880b9fe17f8e9900f2560b9e414b826bbd05
luckytianyiyan <luckytianyiyan@gmail.com>
2016-08-05 00:17:23 +0800

    M Strings add NSLocalizedStringFromTable support

--- tystrings/strings.py
+++ tystrings/strings.py
@@ -2,6 +2,8 @@ import os
 import re
 import codecs
 import subprocess
+import tempfile
+import shutil
 from . import logger
 
 STRING_FILE = 'Localizable.strings'
@@ -12,20 +14,33 @@ class Strings(object):
         self.__dir = target_dir
         self.encoding = encoding
         self.filename = os.path.join(target_dir if target_dir else '', STRING_FILE)
-        self.__reference = {}
+        self.__references = {}
 
     def generate(self, files, update_reference=True):
-        if update_reference:
-            self.update_reference()
         script = 'genstrings'
         for filename in files:
             script += ' %s' % filename
-        self.__run_script('%s -o %s' % (script, self.__dir if self.__dir else '.'))
-        self.__translate()
+
+        logger.done('Generated Strings')
+        temp_dir = tempfile.mkdtemp()
+        self.__run_script('%s -o %s' % (script, temp_dir))
+        logger.debug('')
+        for filename in os.listdir(temp_dir):
+            logger.debug('generated %s' % filename)
+            reference = self.generate_reference(os.path.join(self.__dir if self.__dir else '', filename))
+            self.__references[filename] = reference
+        logger.done('Generated Reference')
+        for k, v in self.__references.items():
+            logger.info('%s count: %d' % (k, len(v)))
+
+        for basename, ref in self.__references.items():
+            target_abspath = os.path.join(self.__dir if self.__dir else '', basename)
+            shutil.copy(os.path.join(temp_dir, basename), target_abspath)
+            self.__translate(target_abspath, ref)
 
     @staticmethod
     def __run_script(script):
-        logger.debug('\nrun: %s' % script)
+        logger.debug('run: %s' % script)
         process = subprocess.Popen(script, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = ''
         while process.poll() is None:
@@ -54,51 +69,37 @@ class Strings(object):
             f.close()
         return reference
 
-    def update_reference(self):
-        self.__reference = {}
-        if os.path.exists(self.filename):
-            f = codecs.open(self.filename, "r", encoding=self.encoding)
-            for line in f:
-                match = re.match(r'"(?P<key>.*?)" = "(?P<value>.*?)";', line)
-                if match is not None:
-                    key = match.group('key')
-                    value = match.group('value')
-                    self.__reference[key] = value
-            f.close()
-        logger.done('Generated Reference')
-        logger.info('count: %r' % len(self.__reference))
-
-    def __translate(self):
+    def __translate(self, dst, reference):
         sum = 0
         translated = {}
         try:
-            f = codecs.open(self.filename, "r", encoding=self.encoding)
+            f = codecs.open(dst, "r", encoding=self.encoding)
             lines = f.readlines()
             for (index, line) in enumerate(lines):
                 match = re.match(r'"(?P<key>.*?)" = "(?P<value>.*?)";', line)
                 if match is not None:
                     key = match.group('key')
-                    result = self.__reference.get(key, None)
+                    result = reference.get(key, None)
 
                     if result is not None:
                         value = match.group('value')
-                        if self.__reference[key] != value:
+                        if reference[key] != value:
                             line = '"%s" = "%s";\n' % (key, result)
                             lines[index] = line
                             sum += 1
                             translated[key] = result
             f.close()
 
-            logger.done('Translated Strings')
-            logger.info('count: %r' % sum)
+            logger.done('Translated: %s' % dst)
+            logger.info('count: %d' % sum)
             logger.debug('')
             for k in translated.keys():
                 logger.debug('%s => %s' % (k, translated[k]))
 
-            f = codecs.open(self.filename, "w+", encoding=self.encoding)
+            f = codecs.open(dst, "w+", encoding=self.encoding)
             f.writelines(lines)
             f.flush()
             f.close()
-            logger.addition('Write strings file to: %s' % self.filename)
+            # logger.addition('Write strings file to: %s' % self.filename)
         except Exception as e:
             logger.error(e)
