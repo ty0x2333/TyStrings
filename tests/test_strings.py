97f0b96790421fc69b2254be04bca701cef113d4
luckytianyiyan <luckytianyiyan@gmail.com>
2016-08-05 23:23:59 +0800

    M test_generate

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -6,17 +6,19 @@ import shutil
 class StringsTest(unittest.TestCase):
     def setUp(self):
         logger.level = logging.DEBUG
-        self.addCleanup(self.cleanup)
-        self.strings = Strings('tests/output')
+        # self.addCleanup(self.cleanup)
 
     def test_generate(self):
-        self.strings.generate(['tests/example/IntegrationDemo.m'])
-        filenames = self.strings.generated_filenames
+        strings = Strings('tests/output/generate')
+        strings.generate(['tests/example/IntegrationDemo.m'])
+        filenames = strings.generated_filenames
         self.assertTrue('Localizable.strings' in filenames)
         self.assertTrue('IntegrationTable.strings' in filenames)
-        self.assertTrue(os.path.exists('tests/output/Localizable.strings'))
-        self.assertTrue(os.path.exists('tests/output/IntegrationTable.strings'))
+        self.assertTrue(os.path.exists('tests/output/generate/Localizable.strings'))
+        self.assertTrue(os.path.exists('tests/output/generate/IntegrationTable.strings'))
 
-    def cleanup(self):
-        if os.path.exists(self.strings.destination):
-            shutil.rmtree(self.strings.destination)
+
+    # def cleanup(self):
+    #     dst = os.path.abspath('tests/output')
+    #     if os.path.exists(dst):
+    #         shutil.rmtree(dst)
