a8136bdd249320848dd9359efac03f8c61da5f54
luckytianyiyan <luckytianyiyan@gmail.com>
2016-08-05 22:12:16 +0800

    F crash when destination strings's directory path not exists

--- tystrings/strings.py
+++ tystrings/strings.py
@@ -32,6 +32,9 @@ class Strings(object):
 
         for basename, ref in self.__references.items():
             target_abspath = os.path.join(self.destination, basename)
+            dirname = os.path.dirname(target_abspath)
+            if not os.path.exists(dirname):
+                os.mkdir(dirname)
             shutil.copy(os.path.join(temp_dir, basename), target_abspath)
             self.__translate(target_abspath, ref)
 
