cc7153e23a3ba2b5a385d501dc2efc53ec969b83
luckytianyiyan <luckytianyiyan@gmail.com>
2017-02-02 02:54:33 +0800

    A BaiduTranslator

---
+++ tystrings/baidu.py
@@ -0,0 +1,30 @@
+import requests
+import random
+import hashlib
+import json
+
+
+class BaiduTranslator(object):
+    def __init__(self, app_id, secret_key, https_enable=True):
+        self.app_id = app_id
+        self.secret_key = secret_key
+        self.api_host = 'https://fanyi-api.baidu.com' if https_enable else 'http://api.fanyi.baidu.com'
+
+    def translate(self, question, dst_lang, src_lang='auto'):
+        result = {}
+        salt = random.randint(32768, 65536)
+        md5 = hashlib.md5()
+        md5.update(self.app_id + question + str(salt) + self.secret_key)
+        sign = md5.hexdigest()
+        params = {'appid': self.app_id,
+                  'q': question,
+                  'from': src_lang,
+                  'to': dst_lang,
+                  'salt': salt,
+                  'sign': sign}
+        response = requests.get(self.api_host + '/api/trans/vip/translate', params=params)
+        response_obj = json.loads(response.text)
+        trans_results = response_obj['trans_result']
+        for trans in trans_results:
+            result[trans['src']] = trans['dst']
+        return result
