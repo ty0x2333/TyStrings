f8746cff1f3fba1750c5b69a9bf2ae7e8fc2cdd5
luckytianyiyan <luckytianyiyan@gmail.com>
2017-02-06 00:04:05 +0800

    M cli

--- tystrings/cli.py
+++ tystrings/cli.py
@@ -9,7 +9,7 @@ from .translator import Translator
 from tabulate import tabulate
 
 
-def parent_paser():
+def parent_parser():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('-v', '--verbose', action="store_true", dest="verbose", help="show more debugging information")
     parser.add_argument('--utf8', action="store_true", dest="utf8", help="use encoding UTF-8")
@@ -31,20 +31,20 @@ def arg_parser():
     parser.add_argument('--version', action='version', version=__version__)
     subparsers = parser.add_subparsers(title='subcommands', dest='action')
 
-    generate_parser = subparsers.add_parser('generate', parents=[parent_paser()],
+    generate_parser = subparsers.add_parser('generate', parents=[parent_parser()],
                                             help='generate `.strings` file from source code files.')
     generate_parser.add_argument('files', metavar='file', nargs='+', help='source file .[mc]')
     generate_parser.add_argument('-a', '--aliases', nargs='+', help='aliases')
     generate_parser.add_argument('-o', '--output', nargs='+', dest='destinations', help='place output files in dirs')
 
-    translate_parser = subparsers.add_parser('translate', parents=[parent_paser()],
+    translate_parser = subparsers.add_parser('translate', parents=[parent_parser()],
                                              help='using Baidu Translate Service to translate `.strings` file.')
     translate_parser.add_argument('source', help='source `.strings` file')
     translate_parser.add_argument('destination', help='destination, a file or directory')
     translate_parser.add_argument('--dst-lang', required=True, help='destination language')
     translate_parser.add_argument('-s', '--src-lang', help='source language')
 
-    lint_parser = subparsers.add_parser('lint', parents=[parent_paser()],
+    lint_parser = subparsers.add_parser('lint', parents=[parent_parser()],
                                              help='Validates a `.strings` file.')
     lint_parser.add_argument('file', help='`.strings` file')
     return parser
